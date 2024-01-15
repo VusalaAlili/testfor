d22ec18251bb0718bb794f292f15960dda43f7ad
Macos <vusalarg@gmail.com>
2024-01-15 06:13:58 +0400

    first commit

---
+++ untitled folder/js/fortest.js
@@ -0,0 +1,28 @@
+
+
+
+// let word = prompt("Text here");
+// let reversed = "";
+// for (let i = word.length - 1; i >= 0; i--) {
+//     reversed += word[i];
+// };
+// alert(reversed);
+
+
+
+
+
+
+// let text = prompt("write text");
+// let letter = prompt("write letter");
+// for (let i = letter.length - 1; i >= 0; --i) {
+//     if (text.indexOf(letter[i]) != -1) {
+//         alert("this letter is used")
+//         break;
+//     }
+//     alert("not found")
+// }
+
+
+
+
